6b24ed2eaa4bbb46bbb4b45e2e22a5a537c60bc8
livinglist <georgefung78@live.com>
2020-03-30 21:54:37 -0700

    Add feature that enables users to upload photos for their posts.

--- lib/resources/repository.dart
+++ lib/resources/repository.dart
@@ -3,7 +3,7 @@ import 'cloud_firestore_provider.dart';
 import 'package:sellit/models/post.dart';
 
 class Repository{
-  Future<List<Post>> fetchAllPosts() async => firestore_provider.fetchAllPosts();
+  Stream<Post> fetchAllPosts() => firestore_provider.fetchAllPosts();
 
   Future uploadPost(Post post) async => firestore_provider.uploadPost(post);
 }
